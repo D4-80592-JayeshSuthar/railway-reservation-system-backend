a9cf6419a61c84fea416d29d899deefc57fb0a10
D4_80288_YashPathak <144443670+IamYashPathak@users.noreply.github.com>
2024-02-05 19:38:36 +0530

    added foriegn keys and mappings to bookigs and tickets pojos

--- railconnect-spring-boot/src/main/java/com/app/entities/Tickets.java
+++ railconnect-spring-boot/src/main/java/com/app/entities/Tickets.java
@@ -1,6 +1,10 @@
 package com.app.entities;
 
+import javax.persistence.Column;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -24,8 +28,17 @@ public class Tickets {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "pnr_number")
 	private Bookings booking;
+	
+	@Column(name = "seat_number", length = 5)
 	private String seatNumber;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status", length = 10)
 	private TicketStatus status;
+	
+	@Column(name = "passenger_name", length = 50)
 	private String passengerName;
+	
+	@Column(name = "aadhar_number", length = 10)
 	private Long aadharNumber;
 }
