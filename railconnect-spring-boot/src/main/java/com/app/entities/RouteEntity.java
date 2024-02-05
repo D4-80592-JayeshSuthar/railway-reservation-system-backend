741c98c3fed140054bdf844511d7f0e93e4c8d09
Meet Vasani <w.vasanimeet@gmail.com>
2024-02-05 16:47:38 +0530

    Added Train ,Coach, Route Entities

---
+++ railconnect-spring-boot/src/main/java/com/app/entities/RouteEntity.java
@@ -0,0 +1,34 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "routes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RouteEntity extends BaseEntity{
+
+    @Column(name = "route_id", unique = true)
+    private int routeId;
+
+    @Column(length = 20)
+    private String source;
+
+    @Column(length = 20)
+    private String destination;
+
+    @Column(length = 20, name = "intermediate")
+    private String intermediate;
+	
+}
