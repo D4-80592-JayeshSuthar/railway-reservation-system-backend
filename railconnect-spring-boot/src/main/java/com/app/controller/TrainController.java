d07f7dd9527a3d3fa090eb97b86ee6a58a1b51b7
Meet Vasani <w.vasanimeet@gmail.com>
2024-02-06 21:58:59 +0530

    added schedule train and cancel train api

--- railconnect-spring-boot/src/main/java/com/app/controller/TrainController.java
+++ railconnect-spring-boot/src/main/java/com/app/controller/TrainController.java
@@ -1,18 +1,27 @@
 package com.app.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.dao.TrainDAO;
 import com.app.dto.TrainDTO;
+import com.app.entities.TrainEntity;
+import com.app.exceptions.ResourceNotFoundException;
 import com.app.service.TrainService;
 
 @RestController
@@ -20,6 +29,9 @@ import com.app.service.TrainService;
 public class TrainController {
 
     private final TrainService trainService;
+    
+    @Autowired
+	private TrainDAO trainDAO;
 
     public TrainController(TrainService trainService) {
         this.trainService = trainService;
@@ -38,10 +50,44 @@ public class TrainController {
         return trainService.addTrain(trainDTO);
     }
 
-    // Admin can cancel trains
-    @DeleteMapping("/cancel/{trainNumber}")
+    
+    //------------------------------------------Old Method---------------------------------------
+//    // Admin can cancel trains
+//    @DeleteMapping("/cancel/{trainNumber}")
+//    //@PreAuthorize("hasRole('ADMIN')")
+//    public void cancelTrain(@PathVariable long trainNumber) {
+//        trainService.cancelTrain(trainNumber);
+//    }
+    
+    //-----------------------------------------------------------------------------
+    
+    // Admin can remove trains (make them inactive)
+    @DeleteMapping("/remove/{trainNumber}")
     //@PreAuthorize("hasRole('ADMIN')")
-    public void cancelTrain(@PathVariable long trainNumber) {
-        trainService.cancelTrain(trainNumber);
+    public void removeTrain(@PathVariable long trainNumber) {
+    	trainService.removeTrain(trainNumber);
     }
+    
+    
+    
+ // Controller method to cancel a train and reschedule it to a particular date
+    @PutMapping("/{trainNumber}/cancel")
+    public ResponseEntity<String> cancelTrainAndReschedule(@PathVariable Long trainNumber, @RequestParam("rescheduleDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate rescheduleDate) {
+        trainService.cancelTrainAndReschedule(trainNumber, rescheduleDate);
+        return ResponseEntity.ok("Train canceled and rescheduled successfully");
+    }
+
+
+    
+    // Controller method to schedule a train after journey completion according to respective days
+    @PutMapping("/{trainNumber}/schedule")
+    public ResponseEntity<String> scheduleTrainAfterJourneyCompletion(@PathVariable Long trainNumber) {
+       
+		TrainEntity trainEntity = trainDAO.findById(trainNumber)
+            .orElseThrow(() -> new ResourceNotFoundException("Train not found"));
+        trainService.scheduleTrainAfterJourneyCompletion(trainNumber, trainEntity.getRunsOn());
+        return ResponseEntity.ok("Train scheduled after journey completion successfully");
+    }
+    
+    
 }
